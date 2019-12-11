9fc680a98ca3c5c2b38802aa2de2bedb27500524
wang <20420162201710@stu.xmu.edu.cn>
2019-12-11 20:23:05 +0800

    1

---
+++ src/main/java/com/example/goods/mapper/BrandMapper.java
@@ -0,0 +1,13 @@
+package com.example.goods.mapper;
+
+import com.example.goods.domain.Brand;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface BrandMapper {
+    public int deleteBrandById(Integer id);
+    public List<Brand> listBrandByCondition();
+    public Brand getBrandById(Integer id);
+}
